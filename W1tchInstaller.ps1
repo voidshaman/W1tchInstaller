cef77e169adcc1f8e23a08c75fa4e0b737870b13
voidshaman <nicolasburaud@gmail.com>
2023-06-03 02:49:00 +0200

    first commit

---
+++ W1tchInstaller.ps1
@@ -0,0 +1,249 @@
+# Check if the script is running with administrator privileges
+
+Write-Host "Checking for administrator privileges..."
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    # Relaunch the script with administrator privileges
+    Write-Host "Launching as Administrator..."
+    Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs
+    Exit
+}
+
+# Define variables
+$DownloadURL = "https://w1tch.net/files/file/1-w1tch-launcher/"
+$InstallPath = "C:\W1tch"
+$ZipFileName = "WLauncher.zip"
+$LauncherName = "WLauncher.exe"
+$UserAgent = "v0id.pw"
+$OutputDirectory = "$env:TEMP\W1tchAutoInstall"
+$ZipPath = "$OutputDirectory\$ZipFileName"
+$Dependencies = @(
+    "C:\Windows\System32\msvcp140.dll", 
+    "C:\Program Files\dotnet\dotnet.exe"
+)
+$DependenciesURL = @(
+    "https://aka.ms/vs/16/release/vc_redist.x64.exe", 
+    "https://download.visualstudio.microsoft.com/download/pr/b70ad520-0e60-43f5-aee2-d3965094a40d/667c122b3736dcbfa1beff08092dbfc3/dotnet-sdk-3.1.426-win-x64.exe"
+)
+#Creating temp directory
+if (-not (Test-Path $OutputDirectory)) {
+    New-Item -ItemType Directory -Path $OutputDirectory | Out-Null
+}
+
+function Test-Dependencies {
+    Write-Host "Checking for dependencies..."
+    for ($i = 0; $i -lt $Dependencies.Count; $i++) {
+        $dep = $Dependencies[$i]
+        if (-not (Test-Path $dep)) {
+            Write-Host "$dep is not installed. Do you want to install it? [Y/N]"
+            $response = Read-Host
+            if ($response -eq "Y") {
+                
+                Start-BitsTransfer -Source $DependenciesURL[$i] -Destination "$OutputDirectory\dep$i.exe"
+                Start-Process -Wait -FilePath "$OutputDirectory\dep$i.exe" -ArgumentList '/norestart'
+                
+                # Check for dotnet.exe
+                if ($dep -eq "C:\Program Files\dotnet\dotnet.exe") {
+                    if (-not (Test-Path $dep)) {
+                        # Look for dotnet.7z
+                        if (Test-Path "C:\Program Files\dotnet\dotnet.7z") {
+                            # Check if 7Zip4Powershell module is installed
+                            if (-not (Get-Module -ListAvailable -Name 7Zip4Powershell)) {
+                                Write-Host "7Zip4Powershell is not installed.`n 7Zip4Powershell is a safe software made to unzip 7zip files with Powershell (https://github.com/thoemmi/7Zip4Powershell) `n Do you want to install it? [Y/N]"
+                                $response = Read-Host
+                                if ($response -eq "Y") {
+                                    Install-Module -Name 7Zip4Powershell -Scope CurrentUser
+                                }
+                                else {
+                                    Write-Host "Continuing without installing 7Zip4Powershell..."
+                                }
+                            }
+                            # Extract dotnet.7z
+                            if (Get-Module -ListAvailable -Name 7Zip4Powershell) {
+                                Write-Host "Extracting dotnet.7z..."
+                                Expand-7Zip "C:\Program Files\dotnet\dotnet.7z" "C:\Program Files\dotnet"
+                            }
+                        }
+                    }
+                }
+            }
+            else {
+                Write-Host "Continuing without installing $dep..."
+            }
+        }
+    }
+}
+
+function Create-Shortcut {
+    param (
+        [string]$TargetPath,
+        [string]$ShortcutPath
+    )
+
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($ShortcutPath)
+    $Shortcut.TargetPath = $TargetPath
+    $Shortcut.Save()
+}
+function Get-Software {
+
+    # Check if the software is already installed
+    if (Test-Path "C:\W1tch\WLauncher.dll") {
+        Write-Host "The software is already installed. Do you want to reinstall? This will delete C:W1tch. [Y/N]"
+        $response = Read-Host
+        if ($response -eq "N") {
+            return $true
+        }
+        else {
+            # Remove existing installation
+            try {
+                Remove-Item "C:\W1tch" -Recurse -Force
+            }
+            catch {
+                Write-Host "Error deleting old installation: $_. Exiting..."
+                return $false
+            }
+        }
+    }
+
+    # Add Windows Defender exclusions if applicable
+    $ExistingExclusions = Get-MpPreference | Select-Object -ExpandProperty ExclusionPath
+    if ($ExistingExclusions -contains $InstallPath) {
+        Write-Host "Exclusions for the installation directory already exist. Moving on..."
+    }
+    else {
+        # Check if Windows Defender is the active antivirus service
+        $defenderStatus = Get-MpComputerStatus
+        if ($defenderStatus.AMServiceEnabled) {
+            $NewExclusions = @(
+                "$InstallPath",
+                "$InstallPath\*",
+                "$InstallPath\*.exe",
+                "$InstallPath\*.dll"
+            )
+            Write-Host "Adding exclusions for Windows Defender..."
+            try {
+                $AllExclusions = $ExistingExclusions + $NewExclusions
+                Set-MpPreference -ExclusionPath $AllExclusions
+            }
+            catch {
+                Write-Host "Error adding exclusions: $_. Moving on..."
+            }
+        }
+        else {
+            Write-Host "Windows Defender is not the active antivirus service. Checking for other antivirus services..."
+    
+            $antivirusProduct = Get-WmiObject -Namespace "root\SecurityCenter2" -Class "AntivirusProduct"
+            $avName = $antivirusProduct.displayName
+            Write-Host "Antivirus software detected: $avName"
+            Write-Host "Skipping exclusion addition as we cannot determine your active antivirus software. 
+        `nYou can find instructions on how to add exclusions for your antivirus software here: 
+        `nhttps://www.google.com/search?q=add+exclusion+$avName" 
+            Write-Host -ForegroundColor Red -BackgroundColor Black "This is a crucial step to ensure that the software works properly."
+        
+        }
+    }
+
+    
+
+    # Download the zip file to a temporary directory
+    if (-not (Test-Path $OutputDirectory)) {
+        New-Item -ItemType Directory -Path $OutputDirectory | Out-Null
+    }
+
+    Write-Host "Downloading software... `n "
+    try {
+        $cli = New-Object System.Net.WebClient
+        $cli.Headers['User-Agent'] = $UserAgent
+        $pageContent = $cli.DownloadString($DownloadURL)
+        $cookies = $cli.ResponseHeaders['Set-Cookie']
+        $datePattern = [Regex]::new('https.+download.+csrfKey=[a-z0-9]{32}')
+        $matche = $datePattern.Match($pageContent)
+        $download = New-Object System.Net.WebClient
+        $download.Headers['User-Agent'] = $UserAgent
+        $download.Headers['Cookie'] = $cookies
+        $url = $matche.Value.Replace('amp;', '')
+        
+        $download.DownloadFile($url, $ZipPath)
+        if (Test-Path $ZipPath) {
+            Write-Host "Download succeeded."
+        }
+        else {
+            Write-Host "Download failed."
+            return $false
+        }
+    }
+    catch {
+        Write-Host "Error downloading file: $_."
+        return $false
+    }
+  
+    # Extract the contents of the zip file to the installation directory
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    Write-Host "Extracting files..."
+    try {
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($ZipPath, $InstallPath)
+        
+    }
+    catch {
+        Write-Host "Error extracting files: $_. Exiting..."
+        Pause
+        Exit
+    }
+    Write-Host "Extraction succeeded. W1tch Launcher is now installed."
+    # Prompt the user if they would like to create a shortcut
+    Write-Host "Do you want to create a desktop shortcut? [Y/N]"
+    $response = Read-Host
+    if ($response -eq "Y") {
+        $DesktopPath = [Environment]::GetFolderPath("Desktop")
+        $ShortcutPath = Join-Path -Path $DesktopPath -ChildPath "WLauncher.lnk"
+        $TargetPath = Join-Path -Path $InstallPath -ChildPath $LauncherName
+        Create-Shortcut -TargetPath $TargetPath -ShortcutPath $ShortcutPath
+        Write-Host "Shortcut created."
+        return $true
+    }
+    else {
+        Write-Host "Have Fun :D"
+    }
+}
+
+
+# Check for dependencies
+Test-Dependencies
+
+Write-Host "Do you want to download and Install the W1tch Launcher? [Y/N]"
+$response = Read-Host
+if ($response -eq "Y") {
+    if (-not (Get-Software)) {
+        Write-Host "Exiting..."
+        Pause
+        Exit
+    }
+}
+else {
+    Write-Host "Exiting..."
+    Pause
+    Exit
+}
+
+# Run the launcher
+Write-Host "Launching software..."
+try {
+    Start-Process "$InstallPath\$LauncherName" -WorkingDirectory $InstallPath
+}
+catch {
+    Write-Host "Error launching software: $_. Exiting..."
+    Pause
+    Exit
+}
+
+# Clean up temporary files
+Write-Host "Cleaning up temporary files..."
+try {
+    Remove-Item $OutputDirectory -Recurse -Force
+}
+catch {
+    Write-Host "Error cleaning up temporary files: $_. Moving on..."
+}
+
+# Pause before exiting
+Pause
